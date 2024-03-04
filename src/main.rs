151a0e739a9b36b5b39049f0a5931ffbff7dac63
Patrick Shelby <patrick.shelby@tatari.tv>
2024-03-04 08:49:35 -0500

    Integration tests!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
+    run(listener)?.await
 }
